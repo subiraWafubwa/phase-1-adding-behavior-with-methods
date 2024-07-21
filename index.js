82eb96120487db3ae1d1a230e913394afffd132b
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-21 21:07:00 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+class Animal {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+}
+
+class Cat extends Animal {
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog extends Animal {
+    speak() {
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird extends Animal {
+    speak() {
+        if (this.gender === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+        } else {
+        return `${this.name} says squawk!`;
+        }
+    }
+}
+
+module.exports = { Cat, Dog, Bird };
